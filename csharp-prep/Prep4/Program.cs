23e11e88d06cd27a521f3fce3d0489a3d9043a5e
Gamerdude1823 <wil18020@byui.edu>
2023-01-11 18:30:38 -0700

    Preps 3,4,5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+ static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> nums = new List<int>();
+        
+        int userNum = -1;
+        while (userNum != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNum = int.Parse(userResponse);
+            
+            if (userNum != 0)
+            {
+                nums.Add(userNum);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in nums)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float ave = ((float)sum) / nums.Count;
+        Console.WriteLine($"The average is: {ave}");
+        
+        int max = nums[0];
+
+        foreach (int num in nums)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
